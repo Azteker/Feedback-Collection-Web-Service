9dbab6136d97ffcbb752ca65fd8187a2c76e9ff3
Yuanxi Zeng <yxzeng2099@gmail.com>
2018-11-21 11:00:17 -0800

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '475879851572-rhk3qdjoler81ntaccqm1nplg3lml8o2.apps.googleusercontent.com',
+  googleClientSecret: 'EcyBu49ZfL8GNpJUtlrK1vvq',
+  mongoURI: 'mongodb://username:benpaoxi45@ds111244.mlab.com:11244/emaily-dev',
+  cookieKey: 'bkmwiuhfbqpkcnugftryo',
+};
--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require ('express');
-const app=express();
+const mongoose = require ('mongoose');
+const cookieSession = require ('cookie-session');
+const passport = require ('passport');
+const keys = require ('./config/keys');
+require ('./models/User');
+require ('./services/passport');
+
+mongoose.connect (keys.mongoURI);
+
+const app = express ();
+
+app.use (
+  cookieSession ({
+    maxAge: 30 * 24 * 3600 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use (passport.initialize ());
+app.use (passport.session ());
+
+require ('./routes/authRoutes') (app);
+
+const PORT = process.PORT || 5000;
+app.listen (PORT);
